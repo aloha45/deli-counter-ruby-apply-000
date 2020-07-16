457f9657066e4814f0e62d4e7cc0c61bf095f78e
Cory Spicer <corys4139@gmail.com>
2020-07-16 20:15:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,10 +12,14 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, new_customer)
-  katz_deli << new_customer
-  puts "Welcome, #{new_customer}. You are number #{katz_deli.length} in line."
-end
+def take_a_number(katz_deli)
+  counter = 0
+  count += 1
+
+# def take_a_number(katz_deli, new_customer)
+#   katz_deli << new_customer
+#   puts "Welcome, #{new_customer}. You are number #{katz_deli.length} in line."
+# end
 
 def now_serving(katz_deli)
   if katz_deli.empty?
